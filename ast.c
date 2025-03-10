cdd36c04212ce3d5fc65ae26b75b99afe47c8147
fred13kim <fred13kim@gmail.com>
2025-03-10 00:45:49 -0400

    add lexical token allocation fns

--- ast.c
+++ ast.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "ast.h"
 
 astnode_t *alloc_astnode(enum nodetype t) {
@@ -15,6 +16,45 @@ astnode_t *alloc_astnode_binop(int op, astnode_t *left, astnode_t *right) {
     return node;
 }
 
-void print_ast(astnode_t *node) {
-
+astnode_t *alloc_astnode_ident(string_t ident) {
+    astnode_t *node = alloc_astnode(AST_IDENT);
+    node->ident.str = ident;
+    return node;
 }
+
+astnode_t *alloc_astnode_number(number_t number) {
+    astnode_t *node = alloc_astnode(AST_NUM);
+    node->num.num = number;
+    return node;
+}
+
+astnode_t *alloc_astnode_charlit(char charlit) {
+    astnode_t *node = alloc_astnode(AST_CHARLIT);
+    node->charlit.char_literal = charlit;
+    return node;
+};
+
+astnode_t *alloc_astnode_string(string_t string) {
+    astnode_t *node = alloc_astnode(AST_STRING);
+    node->str.str = string;
+    return node;
+}
+
+void print_ast(astnode_t *node) {
+    switch(node->type) {
+        case AST_BINOP:
+            break;
+        case AST_CHARLIT:
+            break;
+        case AST_STRING:
+            break;
+        case AST_NUM:
+            break;
+        case AST_IDENT:
+            fprintf(stdout, "IDENT: %s\n", node->str.str.string_literal);
+            break;
+        default:
+            break;
+    }
+}
+
--- ast.h
+++ ast.h
@@ -7,47 +7,51 @@
 typedef struct astnode_s astnode_t;
 
 astnode_t *alloc_astnode_binop(int op, astnode_t *left, astnode_t *right);
+astnode_t *alloc_astnode_ident(string_t ident);
+astnode_t *alloc_astnode_number(number_t number);
+astnode_t *alloc_astnode_charlit(char charlit);
+astnode_t *alloc_astnode_string(string_t string);
 
 void print_ast(astnode_t *node);
 
 enum nodetype{
+    AST_BINOP,
     AST_CHARLIT,
     AST_STRING,
     AST_NUM,
     AST_IDENT,
-    AST_BINOP,
 };
 
-struct astnode_binary {
+struct astnode_binary_s {
     int op;
     astnode_t *left;
     astnode_t *right;
 };
 
-struct astnode_charlit {
+struct astnode_charlit_s {
     char char_literal;
 };
 
-struct astnode_string {
+struct astnode_string_s {
     string_t str;
 };
 
-struct astnode_num {
+struct astnode_num_s {
     number_t num;
 };
 
-struct astnode_ident {
+struct astnode_ident_s {
     string_t str;
 };
 
 struct astnode_s {
     enum nodetype type;
     union {
-        struct astnode_binary  binary;
-        struct astnode_charlit charlit;
-        struct astnode_string  str;
-        struct astnode_num     num;
-        struct astnode_ident   ident;
+        struct astnode_binary_s  binary;
+        struct astnode_charlit_s charlit;
+        struct astnode_string_s  str;
+        struct astnode_num_s     num;
+        struct astnode_ident_s   ident;
     };
 };
 
