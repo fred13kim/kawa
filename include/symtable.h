4c82f1ec7dff1ccbc831be2ae542df76b0be9729
fred13kim <fred13kim@gmail.com>
2025-04-30 22:45:59 -0400

    add attribute declarations

--- include/symtable.h
+++ include/symtable.h
@@ -14,10 +14,62 @@ void symtable_destroy(symtable_t *table);
 symtable_entry_t *lookup(symtable_t *table, symtable_entry_t *entry);
 bool enter(symtable_t *table, symtable_entry_t *entry);
 
+
+struct attr_variable_s {
+
+};
+
+struct attr_function_s {
+
+};
+
+struct attr_struct_union_tag_s {
+
+};
+
+struct attr_enum_tag_s {
+
+};
+
+struct attr_enum_constant_s {
+
+};
+
+struct attr_statement_label_s {
+
+};
+
+struct attr_typedef_name_s {
+
+};
+
+struct attr_struct_union_member_s {
+
+};
+
+
+
+
+
+
 struct symtable_entry_s {
     char *filename;
     int lineno;
 
+    char *name;
+    int namespace;
+
+    int attr_type;
+    union {
+        struct attr_variable_s              variable;
+        struct attr_function_s              function;
+        struct attr_struct_union_tag_s      struct_union_tag;
+        struct attr_enum_tag_s              enum_tag;
+        struct attr_enum_constant_s         enum_constant;
+        struct attr_statement_label_s       statement_label;
+        struct attr_typedef_name_s          typedef_name;
+        struct attr_struct_union_member_s   struct_union_member;
+    };
     /* will contain the following attritubtes:
      * variable
      * function
